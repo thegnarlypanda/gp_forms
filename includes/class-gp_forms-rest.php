df69ba44899c6872d8e4cea9fe6c8c4eda1ee966
Tom <tom.gurney@giantpeach.agency>
2018-01-25 10:10:29 +0000

    Basic File Upload - this might break things

--- includes/class-gp_forms-rest.php
+++ includes/class-gp_forms-rest.php
@@ -41,17 +41,32 @@
                         $tmpValidation = $form['validation'];
                         $tmpErrors = $form['errors'];
                         $errorMessage = $form['mainError'];
-                        if (isset($form['successMessage'])) {
+
+                        if ( isset( $form['files'] ) ) {
+                            $mimetypes = $form['files'];
+                        } else {
+                            $mimetypes = false;
+                        }
+
+                        if ( isset( $form['filesize'] ) ) {
+                            $filesize = $form['filesize'];
+                        } else {
+                            $filesize = false;
+                        }
+
+                        if ( isset( $form['successMessage'] ) ) {
                             $successMessage = $form['successMessage'];
                         } else {
                             $successMessage = 'Form submitted successfully.';
                         }
-                        if (isset($form['email']) && !empty($form['email'])) {
+
+                        if ( isset( $form['email'] ) && !empty( $form['email'] ) ) {
                             $notificationEmail = $form['email'];
                         } else {
                             $notificationEmail = get_option( 'admin_email' );
                         }
-                        if (isset($form['from']) && !empty($form['from']['email']) && !empty($form['from']['name'])) {
+
+                        if ( isset( $form['from'] ) && !empty( $form['from']['email'] ) && !empty( $form['from']['name'] ) ) {
                             $from = array(
                                 'email' => $form['from']['email'],
                                 'name' => $form['from']['name']
@@ -71,9 +86,16 @@
                 );
 
                 try {
-                    $tmpValidation->assert($formData); 
+                    $tmpValidation->assert($formData);
+
+                    if ( isset( $_FILES ) ) {
+                        $files = $this->process_files( $_FILES, $mimetypes, $filesize );
+                    } else {
+                        $files = null;
+                    }
+
                     $this->process_data( $formData, $tmpFields, $formSlug );
-                    $this->notify_admin( $formData, $formName, $notificationEmail, $from );
+                    $this->notify_admin( $formData, $formName, $notificationEmail, $from, $files );
 
                     return array(
                         'hasErrors' => false,
@@ -81,9 +103,9 @@
                     ); 
                 } catch ( NestedValidationException $ex ) {
 
-                    $errorKey = array_keys($tmpErrors);
-                    $errors = $ex->findMessages($errorKey);
-                    $errors = $this->format_error_messages($errors);
+                    $errorKey = array_keys( $tmpErrors );
+                    $errors = $ex->findMessages( $errorKey );
+                    $errors = $this->format_error_messages( $errors );
 
                     return array (
                         'hasErrors' => true,
@@ -94,13 +116,63 @@
             }
         }
 
+        public function process_files( $files, $mimetypes, $filesize ) {
+            if ( ! function_exists( 'wp_handle_upload' ) ) {
+                require_once( ABSPATH . 'wp-admin/includes/file.php' );
+            }
+
+            $keys = array_keys( $files );
+
+            for ( $i = 0; $i < count($keys); $i++ ) {
+                $file = $files[$keys[$i]];              // Current File
+                $correctMime = false;                   // Flag for mime
+                $correctSize = false;                   // Flag for size
+                $uploadedFiles = array();               // Our uploaded files
+
+                if ( $mimetypes ) {
+                    foreach ( $mimetypes as $mimetype ) {
+                        if ( Validator::mimetype( $mimetype )->validate( $file['tmp_name'] ) ) {
+                            $correctMime = true;
+                            break;
+                        }
+                    }
+                }
+                
+                if ( $filesize ) {
+                    if ( Validator::size(null, $filesize)->validate( $file['tmp_name'] ) ) {
+                        $correctSize = true;
+                    }
+                }
+
+                if ( $correctMime && $correctSize ) {   // If these be true, upload the file bruv
+                    $uploaded = wp_handle_upload( $file, array( 'test_form' => false ) );
+
+                    if ( $uploaded && !isset( $uploaded['error'] ) ) {
+                        $uploadedFiles[] = $uploaded['file'];
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+
+            return $uploadedFiles;
+        }
+
+        public function clean_up_files( $files ) {
+            foreach ( $files as $file ) {
+                unlink( $file );
+            }
+        }
+
         public function process_data( $data, $fields, $formSlug ) {
 
             global $wpdb;
             $table_name = $wpdb->prefix . "gp_forms_entires";
             $keys = array_keys( $data );      
 
-            $post_title = $data['name'];
+            $post_title = $data[$keys[0]];  // Set Post Title to first field
             if (isset($data['subject']) && !empty($data['subject'])) $post_title .= " - " . $data['subject'];
 
             $new_id = wp_insert_post( array( 'post_type' => 'gp_forms', 'post_title' => $post_title ) );
@@ -123,7 +195,7 @@
             return false;
         }
 
-        public function notify_admin( $data, $formName, $notificationEmail, $from ) {
+        public function notify_admin( $data, $formName, $notificationEmail, $from, $files ) {
             add_filter( 'wp_mail_content_type', array( $this, "set_to_html" ) );
             
             $headers[] = "From: " . $from['name'] .  " <" . $from['email'] . ">";
@@ -138,9 +210,13 @@
                 $body .=  "<strong>" . $keys[$i] . ":</strong> " . nl2br($data[$keys[$i]]) . "<br>";
             }
 
-            wp_mail( $notificationEmail, $subject, $body, $headers );
+            wp_mail( $notificationEmail, $subject, $body, $headers, $files );
 
             remove_filter( 'wp_mail_content_type', array( $this, "set_to_html" ) );
+
+            if ( $files ) {
+                $this->clean_up_files( $files );
+            }
         }
 
         public function set_to_html() {
